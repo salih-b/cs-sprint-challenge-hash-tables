606c9612b4017c9c09370751b1364196cbe1b2e3
salih <salihmorta17@gmail.com>
2020-08-07 12:58:18 -0400

    completed 3 exersizes

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,27 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    numbers_dict = {}
+    num_of_arr = len(arrays)
+    intersections = []
+    for ray in arrays:
+        for num in ray:
+            if num not in numbers_dict:
+                    numbers_dict[num] = 0
 
-    return result
+            numbers_dict[num] +=1
+            if numbers_dict[num] == num_of_arr:
+                intersections.append(num)
+    
+    return intersections
+
+
+    # get the length of the array of arrays
+    # loop over array or arrays,
+    # loop over array
+    # add each item to the dictionary
+    # check length of array in dictionary if it equals length of
+        # then that item is added to intersection array
 
 
 if __name__ == "__main__":
@@ -15,3 +34,5 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+# testarrays = [[2,4,7,5],[5,7,8],[7,9,5,1]]
+# print(intersection(testarrays))
